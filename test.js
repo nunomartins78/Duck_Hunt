7fe83ebed13ee7007c089178171174b933735371
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-04-17 16:28:05 +0100

    working flapping birds that get shot

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('myCanvas');
-canvas.width = 1000;
-canvas.height = 1000;
+window.innerWidth = screen.availWidth;
+window.innerHeight = screen.availHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
 const ducks = [];
@@ -19,16 +21,40 @@ class Duck {
         this.x = x;
         this.y = y;
         this.gooseWidth = w;
-        this.dirx = Math.random() * 8 - 4;
-        this.diry = Math.random() * 8 - 4;
+        const angle = Math.random() * 2 * Math.PI;
+        const speed = 3; // adjust this to change the speed of the ducks
+        this.dirx = speed * Math.cos(angle);
+        this.diry = speed * Math.sin(angle);
+
+        this.duckDiv = document.createElement('div');
+        this.duckDiv.className = 'duck';
+        document.body.appendChild(this.duckDiv);
+
+        // Set the initial position of the duck using CSS
+        this.duckDiv.style.position = 'absolute';
+        this.duckDiv.style.left = x + 'px';
+        this.duckDiv.style.top = y + 'px';
+        this.duckDiv.style.width = w + 'px';
+        this.duckDiv.style.height = w + 'px';
+
+        this.   duckImg = document.createElement('img');
+        this.duckImg.src = 'images/flyingDuck.gif';
+        this.duckImg.style.width = this.gooseWidth + 'px';
+        this.duckImg.style.height = this.gooseWidth + 'px';
+
+        this.duckDiv.appendChild(this.duckImg);
+
     }
 
     move() {
         this.x += this.dirx;
         this.y += this.diry;
+
+        this.duckDiv.style.left = this.x + 'px';
+        this.duckDiv.style.top = this.y + 'px';
     }
     clickMe(){
-        return (dist(mouseX, mouseY, this.x, this.y)< this.gooseWidth);
+        return (dist(mouseX, mouseY, this.x + this.gooseWidth/2, this.y+ this.gooseWidth/2)< this.gooseWidth/2);
     }
     checkEdges() {
         if (this.x > canvas.width || this.x < 0) {
@@ -39,8 +65,7 @@ class Duck {
         }
     }
     display() {
-        // ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-        ctx.drawImage(duckImg, this.x, this.y, this.gooseWidth, this.gooseWidth);
+        // ctx.drawImage(duckImg, this.x, this.y, this.gooseWidth, this.gooseWidth);
     }
 }
 
@@ -56,20 +81,24 @@ function drawDucks() {
     for (let i = 0; i < ducks.length; i++) {
         ducks[i].move();
         ducks[i].checkEdges();
-        ducks[i].display();
+        // ducks[i].display();
     }
 }
 
 for (let i = 0; i < 20; i++) {
-    ducks.push(new Duck(500, 500, Math.random() * 65 + 10));
+    ducks.push(new Duck(900, 525, 100));
 }
 
-canvas.addEventListener("mousedown", function (event) {
+addEventListener("mousedown", function (event) {
     mouseX = event.clientX - canvas.offsetLeft;
     mouseY = event.clientY - canvas.offsetTop;
+    console.log(mouseX + " & " + mouseY);
     for (var i = ducks.length - 1; i > -1; i--) {
         if (ducks[i].clickMe()) {
+            ducks[i].duckDiv.id = 'dead';
             ducks.splice(i, 1);
+            let deadDucks = document.getElementById("dead");
+            deadDucks.remove();
             break;
         }
     }
